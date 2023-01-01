e7c49f72814c3a6fccd93d444d5a89b9df17991d
Volker Oertel <oertel@hs-albsig.de>
2023-01-01 11:30:25 +0100

    modified:   main.go

--- main.go
+++ main.go
@@ -1,61 +1,60 @@
 package main
 
 import (
-    "encoding/json"
-    "net/http"
-    "strings"
+	"encoding/json"
+	"net/http"
+	"strings"
 )
 
 func main() {
-    http.HandleFunc("/", noop)
-    http.HandleFunc("/hello", hello)
+	http.HandleFunc("/", noop)
+	http.HandleFunc("/hello", hello)
 
-    http.HandleFunc("/weather/", func(w http.ResponseWriter, r *http.Request) {
-        city := strings.SplitN(r.URL.Path, "/", 3)[2]
+	http.HandleFunc("/weather/", func(w http.ResponseWriter, r *http.Request) {
+		city := strings.SplitN(r.URL.Path, "/", 3)[2]
 
-        data, err := query(city)
-        if err != nil {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-            return
-        }
+		data, err := query(city)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 
-        w.Header().Set("Content-Type", "application/json; charset=utf-8")
-        json.NewEncoder(w).Encode(data)
-    })
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		json.NewEncoder(w).Encode(data)
+	})
 
-    http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", nil)
 }
 
 func noop(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Server is running!"))
+	w.Write([]byte("Server is running!"))
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("hello!"))
+	w.Write([]byte("hello!"))
 }
 
 func query(city string) (weatherData, error) {
-    resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?APPID=9a564a62569cb07663ec977ea87e4ff7&q=" + city + "&units=metric")
-    if err != nil {
-        return weatherData{}, err
-    }
+	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?APPID=9a564a62569cb07663ec977ea87e4ff7&q=" + city + "&units=metric")
+	if err != nil {
+		return weatherData{}, err
+	}
 
-    defer resp.Body.Close()
+	defer resp.Body.Close()
 
-    var d weatherData
+	var d weatherData
 
-    if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
-        return weatherData{}, err
-    }
+	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+		return weatherData{}, err
+	}
 
-    return d, nil
+	return d, nil
 }
 
 type weatherData struct {
-    Name string `json:"name"`
-    Main struct {
-        Kelvin float64 `json:"temp"`
-        Pascal float64 `json:"pressure"`
-    } `json:"main"`
+	Name string `json:"name"`
+	Main struct {
+		Kelvin float64 `json:"temp"`
+		Pascal float64 `json:"pressure"`
+	} `json:"main"`
 }
-
